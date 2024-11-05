676eceac13b0120380a2beda492fad047b8ebc36
Aleksei Arapov <arapov-alesha.arapov2015@yandex.ru>
2024-11-05 20:38:30 +0300

    Delete week5 directory

--- week5/task1/A/include/file.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef FILE_HPP
-#define FILE_HPP
-
-#include <string>
-
-bool readFile(const std::string &filename);
-
-#endif // FILE_HPP
-
--- week5/task1/B/include/shannon_entropy.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef SHANNON_ENTROPY_HPP
-#define SHANNON_ENTROPY_HPP
-
-double calculateShannonEntropy(int frequency[], int totalChars);
-
-#endif // SHANNON_ENTROPY_HPP
-
-
